ad557ec405d7f6b5c33ab01ccef697939bf920af
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2023-10-18 10:37:51 +0200

    feat: changed vue style

--- LuminAi/src/main/frontend/src/components/HelloWorld.vue
+++ LuminAi/src/main/frontend/src/components/HelloWorld.vue
@@ -31,12 +31,7 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
