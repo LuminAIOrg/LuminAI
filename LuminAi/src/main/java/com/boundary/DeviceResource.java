a4e1bb0c8775316f3b3d098efff3e980c1141c06
LukMayr <l.mayrhuber@students.htl-leonding.ac.at>
2023-10-23 12:15:13 +0200

    feat: read data from json and send it to endpoint

---
+++ LuminAi/src/main/java/com/boundary/DeviceResource.java
@@ -0,0 +1,43 @@
+package com.boundary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vertx.core.json.Json;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+
+import java.io.File;
+import java.net.URL;
+
+@Path("/api/devices")
+public class DeviceResource {
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        @Path("/getData")
+        public String getData(){
+            try {
+                String pythonScript = "python";
+                URL scriptPath = DeviceResource.class.getResource("drivers/divers.py");
+
+                System.out.println(scriptPath);
+
+                ProcessBuilder processBuilder = new ProcessBuilder(pythonScript);
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+                System.out.println("Python script exit code: " + exitCode);
+
+                //TODO: map json file to object
+
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                URL filePath = DeviceResource.class.getResource("/drivers/energy_data.json");
+
+                Object object = objectMapper.readValue(filePath, Object.class);
+                return objectMapper.writeValueAsString(object);
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return null;
+        }
+
+}
