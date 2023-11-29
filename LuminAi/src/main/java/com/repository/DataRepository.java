331154491144ed9de89002d3d8f0917bbbe5da8f
Stenz123 <michael.stenz2006@gmail.com>
2023-11-29 20:59:19 +0100

    #42 add missing @Transactional

--- LuminAi/src/main/java/com/repository/DataRepository.java
+++ LuminAi/src/main/java/com/repository/DataRepository.java
@@ -4,6 +4,7 @@ import com.model.Data;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -12,10 +13,12 @@ public class DataRepository {
     @Inject
     EntityManager entityManager;
 
+    @Transactional
     public void addData(Data data) {
         entityManager.persist(data);
     }
 
+    @Transactional
     public void addData(List<Data> data) {
         if (data != null)  {
             data.forEach(d -> entityManager.persist(d));
