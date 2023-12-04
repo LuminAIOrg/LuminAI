90c077d5f680a601a9ed3112ab69b263d538396f
JakSchlager <jami.schlager@gmail.com>
2023-12-05 00:26:31 +0100

    feat: #26 added some commented out code to work on later (not working yet)

--- LuminAi/src/main/frontend/src/testingChartJs/DeviceDataChart.vue
+++ LuminAi/src/main/frontend/src/testingChartJs/DeviceDataChart.vue
@@ -12,12 +12,60 @@
   </Chart>
 </template>
 
+
 <script>
 import { defineComponent } from 'vue'
 import { Chart, Grid, Line } from 'vue3-charts'
 import { computed } from 'vue'
 import {store} from "@/store/Store";
 
+/*
+import Chart from 'chart.js/auto'
+export default {
+  updateChart() {
+    console.log("bsihadbfojabsdb")
+    const name = computed(() => store.deviceData.map(entry => entry.name))
+    const timestamp = computed(() => store.deviceData.map(entry => entry.timestamp))
+    const value = computed(() => store.deviceData.map(entry => entry.value))
+
+    const data = {
+      labels: timestamp,
+      datasets: [{
+        label: name,
+        borderColor: [
+          'rgba(255, 102, 102)',
+          'rgba(255, 178, 102)',
+          'rgba(178, 255, 102)',
+          'rgba(102, 255, 255)',
+          'rgba(102, 102, 255)',
+          'rgba(255, 102, 255)',
+          'rgba(255, 102, 178)'
+        ],
+        data: value,
+        borderWidth: 1
+      }]
+    };
+    const config = {
+      type: 'line',
+      data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+    const energyOverviewChart = new Chart(
+        document.getElementById('energyOverviewChart'),
+        config
+    );
+    energyOverviewChart;
+  }
+};
+
+updateChart();
+*/
 
 export default defineComponent({
   name: 'DeviceDataChart',
@@ -28,6 +76,8 @@ export default defineComponent({
   },
   setup() {
     const data = computed(() => store.deviceData.map(entry => ({
+      //date: new Date(parseInt(entry.timestamp) * 1000),
+      //formattedDate: date.toLocaleDateString('de-DE'),
       timestamp: entry.timestamp,
       value: entry.value,
     })))
@@ -38,4 +88,5 @@ export default defineComponent({
     return { data, margin, direction }
   },
 });
+
 </script>
