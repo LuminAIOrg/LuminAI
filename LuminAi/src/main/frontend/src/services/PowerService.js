9d8f1523bcae52fd26ef9e3cbac0b2f5e5edc5d4
Michi PC <michael.ruep@gmail.com>
2023-12-05 09:44:18 +0100

    fix: added handling disconnects (not only errors)

--- LuminAi/src/main/frontend/src/services/PowerService.js
+++ LuminAi/src/main/frontend/src/services/PowerService.js
@@ -7,6 +7,10 @@ socket.on('connect', () =>{
     console.log(socket);
 });
 
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
+
 socket.on('connect_error', (error) => {
     console.error('Connection error:', error);
 });
