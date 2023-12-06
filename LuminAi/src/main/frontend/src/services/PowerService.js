582b325263432d8dc33983afc89865821ef6fa27
Michi PC <michael.ruep@gmail.com>
2023-12-06 11:51:01 +0100

    fix: added a function which can be called once a connection is needed #43

--- LuminAi/src/main/frontend/src/services/PowerService.js
+++ LuminAi/src/main/frontend/src/services/PowerService.js
@@ -3,18 +3,21 @@ import {io} from "socket.io-client";
 
 const socket = io();
 
-socket.on('connect', () => {
-    console.log('Connected to server');
+export function startSocketClient(){
+    socket.on('connect', () => {
+        console.log('Connected to server');
 
-    socket.on('/subscribeUpdates', (data) => {
-        store.deviceData = data;
+        socket.on('/subscribeUpdates', (data) => {
+            console.log(data);
+            store.deviceData = data;
+        });
     });
-});
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-});
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+    });
 
-socket.on('connect_error', (error) => {
-    console.error('Connection error:', error);
-});
+    socket.on('connect_error', (error) => {
+        console.error('Connection error:', error);
+    });
+}
