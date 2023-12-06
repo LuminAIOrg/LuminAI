1d02623659ef075e375df0f2118eb0619ff2a4cc
Michi PC <michael.ruep@gmail.com>
2023-12-06 13:13:46 +0100

    fix: frontend socket now working and tested

--- LuminAi/src/main/frontend/src/services/PowerService.js
+++ LuminAi/src/main/frontend/src/services/PowerService.js
@@ -1,13 +1,23 @@
 import { store } from "@/store/Store";
 
-const socket = new WebSocket('ws://localhost');
+const socket = new WebSocket('ws://localhost:8080/subscribeUpdates');
 
 export function startSocketClient() {
-    socket.addEventListener('open', () => {
-        console.log('Connected to server');
+    socket.onopen = function (event) {
+        console.log("WebSocket connection opened:", event);
+    };
 
-        socket.send(JSON.stringify({
-            event: 'subscribeUpdates'
-        }));
-    });
+    socket.onmessage = function (event) {
+        console.log(event.data);
+        const data = JSON.parse(event.data);
+        store.deviceData = data;
+    };
+
+    socket.onclose = function (event) {
+        console.log("WebSocket connection closed:", event);
+    };
+
+    socket.onerror = function (error) {
+        console.error("WebSocket error:", error);
+    };
 }
