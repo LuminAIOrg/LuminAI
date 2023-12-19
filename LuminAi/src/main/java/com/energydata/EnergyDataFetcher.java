fec55ed1ad79b777480fd3a92007f9dd1b47dfc7
Michi PC <michael.ruep@gmail.com>
2023-12-19 08:16:48 +0100

    fix: minor commit to save work

--- LuminAi/src/main/java/com/energydata/EnergyDataFetcher.java
+++ LuminAi/src/main/java/com/energydata/EnergyDataFetcher.java
@@ -7,7 +7,70 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.Gson;
+
+/*
+{
+   "Body" : {
+      "Data" : {
+         "Inverters" : {
+            "1" : {
+               "Battery_Mode" : "nearly depleted",
+               "DT" : 1,
+               "E_Day" : null,
+               "E_Total" : 15939732.744999999,
+               "E_Year" : null,
+               "P" : 656.4710693359375,
+               "SOC" : 3.2999999999999998
+            }
+         },
+         "SecondaryMeters" : {
+            "1" : {
+               "Category" : "METER_CAT_HEATPUMP",
+               "Label" : "Wärmepumpe",
+               "MLoc" : 256.0,
+               "P" : -1367.0999999999999
+            },
+            "2" : {
+               "Category" : "METER_CAT_WR",
+               "Label" : "Balkonkraftwerk",
+               "MLoc" : 3.0,
+               "P" : 95.700000000000003
+            }
+         },
+         "Site" : {
+            "BackupMode" : false,
+            "BatteryStandby" : false,
+            "E_Day" : null,
+            "E_Total" : 15939732.744999999,
+            "E_Year" : null,
+            "Meter_Location" : "grid",
+            "Mode" : "bidirectional",
+            "P_Akku" : -8.4001226425170898,
+            "P_Grid" : 871.60000000000002,
+            "P_Load" : -1622.2798583984377,
+            "P_PV" : 707.19361877441406,
+            "rel_Autonomy" : 46.273141746303303,
+            "rel_SelfConsumption" : 100.0
+         },
+         "Smartloads" : {
+            "Ohmpilots" : {}
+         },
+         "Version" : "12"
+      }
+   },
+   "Head" : {
+      "RequestArguments" : {},
+      "Status" : {
+         "Code" : 0,
+         "Reason" : "",
+         "UserMessage" : ""
+      },
+      "Timestamp" : "2023-12-15T08:44:33+00:00"
+   }
+}
+ */
 
 public class EnergyDataFetcher {
 
