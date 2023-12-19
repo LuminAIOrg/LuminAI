220fb909128f0e6a824c0c7742f42c4689e3c6c3
Michi PC <michael.ruep@gmail.com>
2023-12-19 10:18:32 +0100

    add: DataEncoder for sending data via Sockets

---
+++ LuminAi/src/main/java/com/data/session/DataEncoder.java
@@ -0,0 +1,24 @@
+package com.data.session;
+
+import com.data.model.Data;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.websocket.EncodeException;
+import jakarta.websocket.Encoder;
+import jakarta.websocket.EndpointConfig;
+
+public class DataEncoder implements Encoder.Text<Data> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String encode(Data data) throws EncodeException {
+        try {
+
+            return objectMapper.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            throw new EncodeException(data, "Error encoding Data object to JSON", e);
+        }
+    }
+
+}
