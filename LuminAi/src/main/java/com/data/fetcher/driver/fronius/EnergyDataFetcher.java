5906ed79863477624aaf2d69eb751fc980bf4d64
Michi <michael.ruep@gmail.com>
2024-01-10 09:15:18 +0100

    fix: EnergyDataFetcher still need repo to complete this code commiting for saving #69 #38

--- LuminAi/src/main/java/com/data/fetcher/driver/fronius/EnergyDataFetcher.java
+++ LuminAi/src/main/java/com/data/fetcher/driver/fronius/EnergyDataFetcher.java
@@ -1,8 +1,11 @@
-package com.data.fetcher.driver.fonius;
+package com.data.fetcher.driver.fronius;
 
 import com.data.fetcher.driver.Driver;
-import com.data.model.Data;
 import com.google.gson.Gson;
+import com.model.Group;
+import com.model.Sensor;
+import com.model.SensorData;
+import io.quarkus.logging.Log;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -29,7 +32,7 @@ public class EnergyDataFetcher extends Driver {
         }
     }
 
-    public List<Data> runDriver() throws IOException {
+    public SensorData runDriver() throws IOException {
         String dataJson = fetchDataFromApi();
 
         Gson gson = new Gson();
@@ -39,43 +42,66 @@ public class EnergyDataFetcher extends Driver {
         Map<String, Object> dataProperty = (Map<String, Object>) body.get("Data");
         Map<String, Object> site = (Map<String, Object>) dataProperty.get("Site");
 
-        Map<String, Object> energyData = new HashMap<>();
-        energyData.put("devices", new ArrayList<>());
-        List<Map<String, Object>> devices = (List<Map<String, Object>>) energyData.get("devices");
+        // innit Group
+        //TODO: check if group already exists (i need repository :c)
+        Group fronius = new Group();
+        fronius.setName("Fronius");
+
+        //Sensors
+        Sensor pvSensor = new Sensor();
+        pvSensor.setName("PV");
+
+        Sensor gridSensor = new Sensor();
+        gridSensor.setName("Grid");
+
+        Sensor akkuSensor = new Sensor();
+        akkuSensor.setName("Akku");
+
+        //TODO: i still need timestamp but there is no timestamp in
 
         // PV
-        Map<String, Object> pvData = new HashMap<>();
-        pvData.put("name", "PV");
-        Double pvValue = (Double) site.get("P_PV");
-        pvData.put("value", pvValue);
-        devices.add(pvData);
-
-        // Grid
-        Map<String, Object> gridData = new HashMap<>();
-        gridData.put("name", "Grid");
-        Double gridValue = (Double) site.get("P_Grid");
-        gridData.put("value",gridValue );
-        devices.add(gridData);
-
-        Map<String, Object> akkuData = new HashMap<>();
-        akkuData.put("name", "Akku");
-        Double akkuValue = (Double) site.get("P_Akku");
-        akkuData.put("value",akkuValue );
-        devices.add(akkuData);
-
-
-        Map<String, Object> secondaryMeters = (Map<String, Object>) (dataProperty.get("SecondaryMeters"));
-        for (Map.Entry<String, Object> entry : secondaryMeters.entrySet()) {
-            Map<String, Object> entryData = (Map<String, Object>) entry.getValue();
-            Map<String, Object> deviceData = new HashMap<>();
-            deviceData.put("name", entryData.get("Label"));
-            deviceData.put("value", entryData.get("P"));
-            devices.add(deviceData);
+        SensorData pvData = new SensorData();
+        try{
+            pvData.setValue((Double) site.get("P_PV"));
+        }catch (Exception exception){
+            Log.info("There is no PV-Data in this Fronius API!");
         }
 
-        energyData.put("timestamp", ((Map<String, Object>) data.get("Head")).get("Timestamp"));
+        // Grid
+        SensorData gridData = new SensorData();
+        try{
+            gridData.setValue((Double) site.get("P_Grid"));
+        }catch (Exception exception){
+            Log.info("There is no Grid-Data in this Fronius API!");
+        }
+
+        //Akku
+        SensorData akkuData = new SensorData();
+        try{
+            akkuData.setValue((Double) site.get("P_Akku"));
+        }catch (Exception exception){
+            Log.info("There is no Akku-Data in this Fronius API!");
+        }
+
+        //TODO: try catch in loop
+        Map<String, Object> secondaryMeters = (Map<String, Object>) (dataProperty.get("SecondaryMeters"));
+        for (Map.Entry<String, Object> entry : secondaryMeters.entrySet()) {
+                Map<String, Object> entryData = (Map<String, Object>) entry.getValue();
+                Map<String, Object> deviceData = new HashMap<>();
+
+                //TODO: check if sensor already exists (i need repository :c)
+                Sensor sensor = new Sensor();
+                sensor.setName(String.valueOf(entryData.get("Label")));
+
+                SensorData sensorData = new SensorData();
+                sensorData.setValue((Double) entryData.get("P"));
+
+                fronius.addSensor(sensor);
+                sensor.addValue(sensorData);
+        }
 
         //TODO Impliment Driver to return Data object
-        return new ArrayList<>();
+        //TODO: maybe return an feichtn schaß
+        return new SensorData();
     }
 }
