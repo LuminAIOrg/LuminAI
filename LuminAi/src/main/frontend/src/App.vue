0852a0f44d631fcd1ef30c723f6d2999c82ac06a
jschlgr <jami.schlager@gmail.com>
2024-02-13 08:08:14 +0100

    fix: frontend design

--- LuminAi/src/main/frontend/src/App.vue
+++ LuminAi/src/main/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
   </div>
-  <div class="w-screen flex justify-evenly relative top-10">
+  <div class="w-screen flex justify-evenly relative top-12">
     <ChartComponent device_name="Solar" border_color="rgb(255, 0, 0, 1)" device_unit="Temperature in °C"></ChartComponent>
     <ChartComponent device_name="Wärmepumpe" border_color="rgb(0, 0, 255, 1)" device_unit="kW"></ChartComponent>
   </div>
