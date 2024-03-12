0d86ea55239747afc63a16d95e8293a875a0df69
Michi4 <michael.ruep@gmail.com>
2024-03-12 08:45:16 +0100

    fix: minor testing and fixing

--- LuminAi/src/main/java/com/data/utils/Store.java
+++ LuminAi/src/main/java/com/data/utils/Store.java
@@ -2,6 +2,7 @@ package com.data.utils;
 
 import com.data.model.Sensor;
 import com.data.model.SensorData;
+import com.data.model.SensorDataId;
 import com.data.repository.GroupRepository;
 import com.data.repository.SensorDataRepository;
 import com.data.repository.SensorRepository;
@@ -13,6 +14,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.context.ManagedExecutor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @ApplicationScoped
@@ -31,11 +34,13 @@ public class Store {
     ManagedExecutor managedExecutor;
 
     BehaviorSubject<SensorData> subject;
-    Consumer<SensorData> recipient;
+
+    BehaviorSubject<List<SensorData>> subjectList;
 
     @Inject
     public Store() {
         this.subject = BehaviorSubject.createDefault(new SensorData());
+        this.subjectList = BehaviorSubject.createDefault(new ArrayList<SensorData>());
     }
 
     @Inject
@@ -43,6 +48,10 @@ public class Store {
 
     public BehaviorSubject<SensorData> getSubject() {
         return subject;
+    }
+
+    public BehaviorSubject<List<SensorData>> getSubjectList() {
+        return subjectList;
     }
 
     public void next() {
@@ -61,13 +70,43 @@ public class Store {
         //}
     }
 
+    public void addAll() {
+        if(subjectList.getValue() == null){
+            Log.info("Data List is null!");
+            return;
+        }
+        for(SensorData sensorData : subjectList.getValue()){
+            if (sensorData == null) {
+                Log.warn("Sensor Data SensorData is null");
+            } else {
+                checkAndPersistData(sensorData);
+                dataSocket.publish(sensorData);
+            }
+            //try {
+            //    TODO: get Recipient
+            //    recipient.accept(sensorData);
+            //}catch (Throwable e) {
+            //    throw new RuntimeException("can't pe accepted by recipient \n" + e.getMessage());
+            //}
+        }
+    }
+
     private void checkAndPersistData(SensorData sensorData) {
-        System.out.println(sensorData.getValue());
         Sensor mergedSensor = this.sensorRepository.createOrGetSensor(sensorData.getSensor().getName());
         sensorData.setSensor(mergedSensor);
         //TODO: Add group support
         //Group mergedGroup = this.groupRepository.createOrGetGroup(sensorData.getSensor().getGroup().getName());
         //sensorData.getSensor().setGroup(mergedGroup);
+        //System.out.println("gibts eh scho oda soÖLSDKJFSLÖDFJDFÖLS: " + mergedSensor.getValues().stream().anyMatch(sd -> sd.getSensorDataId().getTimestamp() == sensorData.getSensorDataId().getTimestamp()));
         this.sensorDataRepository.addData(sensorData);
+        /*
+        if (!mergedSensor.getValues().stream()
+                .anyMatch(sd -> sd.getSensorDataId().getTimestamp() == sensorData.getSensorDataId().getTimestamp())) {
+            System.out.println("SensorData added! TODO: check timestamp only.");
+
+        } else {
+            System.out.println("SensorData already exists! TODO: check timestamp only.");
+        }
+        */
     }
 }
