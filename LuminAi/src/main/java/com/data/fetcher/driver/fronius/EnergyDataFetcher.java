29bdbe6774661149587eef046232650ab418e9e9
Michi4 <michael.ruep@gmail.com>
2024-03-12 08:48:20 +0100

    fix: major changes fixed everything + scheduling and whole driver now properly working #119

--- LuminAi/src/main/java/com/data/fetcher/driver/fronius/EnergyDataFetcher.java
+++ LuminAi/src/main/java/com/data/fetcher/driver/fronius/EnergyDataFetcher.java
@@ -3,6 +3,7 @@ package com.data.fetcher.driver.fronius;
 import com.data.model.Group;
 import com.data.model.Sensor;
 import com.data.model.SensorData;
+import com.data.model.SensorDataId;
 import com.data.spi.FetcherType;
 import com.data.spi.ServiceInterface;
 import com.data.utils.PropLoader;
@@ -13,14 +14,19 @@ import io.quarkus.logging.Log;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class EnergyDataFetcher implements ServiceInterface {
 
-    List<SensorData> dataList;
     private Store store;
     private Properties properties;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     private static String fetchDataFromApi(String apiUrl) throws IOException {
         URL url = new URL(apiUrl);
@@ -42,9 +48,16 @@ public class EnergyDataFetcher implements ServiceInterface {
         }
     }
 
-    public /*List<SensorData>*/ void runDriver() throws IOException {
-        String apiURL = properties.getProperty("apiURL");
-        String dataJson = fetchDataFromApi(apiURL);
+    public /*List<SensorData>*/ void runDriver() throws RuntimeException {
+        String apiUrl = properties.getProperty("apiUrl");
+        String dataJson;
+
+        try {
+            dataJson = fetchDataFromApi(apiUrl);
+        } catch (IOException e) {
+            Log.info("Failed when fetching data from API! Maybe on wrong Network? :) Please Check if you can reach: " + apiUrl);
+            throw new RuntimeException("Failed when fetching data from API: " + e);
+        }
 
         Gson gson = new Gson();
         Map<String, Object> data = gson.fromJson(dataJson, Map.class);
@@ -52,6 +65,10 @@ public class EnergyDataFetcher implements ServiceInterface {
         Map<String, Object> body = (Map<String, Object>) data.get("Body");
         Map<String, Object> dataProperty = (Map<String, Object>) body.get("Data");
         Map<String, Object> site = (Map<String, Object>) dataProperty.get("Site");
+
+        Map<String, Object> head = (Map<String, Object>) data.get("Head");
+
+        List<SensorData> dataList = new ArrayList<>();
 
         // innit Group
         Group fronius = new Group("Fronius");
@@ -61,25 +78,47 @@ public class EnergyDataFetcher implements ServiceInterface {
         Sensor gridSensor = new Sensor("Grid");
         Sensor akkuSensor = new Sensor("Akku");
 
+
+        //TODO:ob do oda so wiad nix mehr ausgeführt ka numoi schaun von wo genau
+
         //TODO:
         pvSensor.setGroup(fronius);
         gridSensor.setGroup(fronius);
         akkuSensor.setGroup(fronius);
 
+        String dateString = (String) head.get("Timestamp");
+
+        // Parse the string to OffsetDateTime
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
+        // Convert OffsetDateTime to long timestamp
+        long timestamp = offsetDateTime.toInstant().toEpochMilli();
+
         // PV
-        SensorData pvData = new SensorData();
-        pvData.setSensor(pvSensor);
         try {
+            if((Double) site.get("P_PV") == null) throw new RuntimeException();
+            SensorDataId pvSensorDataId = new SensorDataId();
+            pvSensorDataId.setTimestamp(timestamp);
+
+            SensorData pvData = new SensorData();
+            pvData.setSensorDataId(pvSensorDataId);
+            pvData.setSensor(pvSensor);
+
             pvData.setValue((Double) site.get("P_PV"));
             dataList.add(pvData);
         } catch (Exception exception) {
             Log.info("There is no PV-Data in this Fronius API!");
         }
-
         // Grid
-        SensorData gridData = new SensorData();
-        gridData.setSensor(gridSensor);
         try {
+            if((Double) site.get("P_Grid") == null) throw new RuntimeException();
+            SensorDataId gridSensorDataId = new SensorDataId();
+            gridSensorDataId.setTimestamp((timestamp));
+
+            SensorData gridData = new SensorData();
+            gridData.setSensorDataId(gridSensorDataId);
+            gridData.setSensor(gridSensor);
+
             gridData.setValue((Double) site.get("P_Grid"));
             dataList.add(gridData);
         } catch (Exception exception) {
@@ -87,34 +126,45 @@ public class EnergyDataFetcher implements ServiceInterface {
         }
 
         //Akku
-        SensorData akkuData = new SensorData();
-        akkuData.setSensor(akkuSensor);
         try {
+            if((Double) site.get("P_Akku") == null) throw new RuntimeException();
+            SensorDataId akkuSensorDataId = new SensorDataId();
+            akkuSensorDataId.setTimestamp((timestamp));
+
+            SensorData akkuData = new SensorData();
+            akkuData.setSensorDataId(akkuSensorDataId);
+            akkuData.setSensor(akkuSensor);
+
             akkuData.setValue((Double) site.get("P_Akku"));
         } catch (Exception exception) {
             Log.info("There is no Akku-Data in this Fronius API!");
         }
-
         //TODO: try catch in loop
-        Map<String, Object> secondaryMeters = (Map<String, Object>) (dataProperty.get("SecondaryMeters"));
-        for (Map.Entry<String, Object> entry : secondaryMeters.entrySet()) {
-            Map<String, Object> entryData = (Map<String, Object>) entry.getValue();
-            Map<String, Object> deviceData = new HashMap<>();
+        try {
+            Map<String, Object> secondaryMeters = (Map<String, Object>) (dataProperty.get("SecondaryMeters"));
+            for (Map.Entry<String, Object> entry : secondaryMeters.entrySet()) {
+                Map<String, Object> entryData = (Map<String, Object>) entry.getValue();
 
-            //Create or Get Sensor
-            Sensor sensor = new Sensor(String.valueOf(entryData.get("Label")));
-            sensor.setGroup(fronius);
+                //Create or Get Sensor
+                Sensor sensor = new Sensor(String.valueOf(entryData.get("Label")));
+                sensor.setGroup(fronius);
 
-            SensorData sensorData = new SensorData();
-            sensorData.setSensor(sensor);
-            sensorData.setValue((Double) entryData.get("P"));
-            dataList.add(sensorData);
+                SensorDataId sensorDataId = new SensorDataId();
+                sensorDataId.setTimestamp((timestamp));
 
-            //set connections
-            store.getSubject().onNext(sensorData);
-            store.next();
+                SensorData sensorData = new SensorData();
+                sensorData.setSensorDataId(sensorDataId);
+                sensorData.setSensor(sensor);
+                sensorData.setValue((Double) entryData.get("P"));
+
+                //set connections
+                dataList.add(sensorData);
+            }
+        } catch (Exception e){
+            Log.info("Secondary Meters does not exist in this Fronius API: " + e);
         }
-        //return dataList;
+        store.getSubjectList().onNext(dataList);
+        store.addAll();
     }
 
     @Override
@@ -131,13 +181,12 @@ public class EnergyDataFetcher implements ServiceInterface {
 
     @Override
     public CompletableFuture<Void> invoke() {
-        System.out.println("Driver got Invoked");
         if (properties != null) {
-            System.out.println("properties set");
             return CompletableFuture.runAsync(() -> {
                 try {
-                    runDriver();
-                } catch (IOException e) {
+                    scheduler.scheduleAtFixedRate(this::runDriver, 0, 2, TimeUnit.SECONDS);
+                } catch (RuntimeException e) {
+                    Log.info("OIS OASCH");
                     throw new RuntimeException(e);
                 }
             });
