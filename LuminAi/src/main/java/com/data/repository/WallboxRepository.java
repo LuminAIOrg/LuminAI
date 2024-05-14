b0284b22a664b0064773a86f2cb3aa65ac7ee04d
Michi4 <michael.ruep@gmail.com>
2024-05-14 09:33:30 +0200

    add: repo for mangaging data #148

---
+++ LuminAi/src/main/java/com/data/repository/WallboxRepository.java
@@ -0,0 +1,47 @@
+package com.data.repository;
+
+import com.data.dto.DataDto;
+import com.data.dto.SensorWithoutDataDto;
+import com.data.model.Sensor;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class WallboxRepository {
+
+    @Inject
+    EntityManager entityManager;
+
+    public List<SensorWithoutDataDto> getAllSensors() {
+        return this.entityManager
+                .createQuery("select new com.data.dto.SensorWithoutDataDto(s.id, s.name, s.unit) from Sensor s", SensorWithoutDataDto.class)
+                .getResultList();
+    }
+
+    public List<DataDto> getAllDataFromSensor(long sensorId) {
+        return entityManager
+                .createQuery("SELECT new com.data.dto.DataDto(d.sensorDataId.timestamp, d.value) FROM SensorData d WHERE d.sensor.id = :sensorId", DataDto.class)
+                .setParameter("sensorId", sensorId)
+                .getResultList();
+    }
+
+    @Transactional
+    public Sensor createOrGetSensor(String sensorName) {
+        List<Sensor> sensor = entityManager.createQuery("SELECT s FROM Sensor s WHERE s.name = :sensorName", Sensor.class)
+                .setParameter("sensorName", sensorName)
+                .getResultList();
+
+        if (sensor.isEmpty()) {
+            Sensor newSensor = new Sensor();
+            newSensor.setName(sensorName);
+            entityManager.merge(newSensor);
+            entityManager.flush();
+            return newSensor;
+        }
+        return sensor.get(0);
+    }
+}
